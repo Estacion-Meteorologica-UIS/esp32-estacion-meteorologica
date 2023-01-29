3b999f6458d27a69f74556efac4d2c0feccd50ea
Konat Sebastian <juansebastianguerrero2001@gmail.com>
2023-01-28 19:19:11 -0500

    Se le dio formato a la informacion del codigo, se modifico el imprimir como funcion, se agrego codigo de erro. Pendiente por testear en circuito

--- Sensores/temperatura y humedad/sensorhum/sensorhum.ino
+++ Sensores/temperatura y humedad/sensorhum/sensorhum.ino
@@ -1,6 +1,22 @@
+/////////////////////////////////////////////////////////////////////////////
+// Sharp DHT11 Sensor de Temperatura y Humedad
+//
+//Tiempo estimado de ejecucion: +2 Segs
+//
+// Board Connection: https://i0.wp.com/randomnerdtutorials.com/wp-content/uploads/2019/04/dht_esp32_bb.png?w=714&quality=100&strip=all&ssl=1
+//   
+//
+// Serial monitor setting:
+//   9600 baud
+/////////////////////////////////////////////////////////////////////////////
+// Codigo base de: https://randomnerdtutorials.com/esp32-dht11-dht22-temperature-humidity-sensor-arduino-ide/
+// Codigo dado por: Daniel Cerro
+// Codigo modificado por: Juan Sebastian Guerrero
+/////////////////////////////////////////////////////////////////////////////
+
 #include <DHT.h>
 #include <DHT_U.h>
-#include <dummy.h>
+//#include <dummy.h>
 
 #define DHTPIN 4
 #define DHTTYPE DHT11
@@ -14,7 +30,7 @@ Serial.println(F("DHTxx test!"));
 dht.begin();
 }
 
-void imprimirenpantalla() {	
+void imprimirTyH(float t, float h) {	
 	
 	if(isnan(h) || isnan(t)){
 		Serial.println(F("Failed to read from DHT sensor!"));
@@ -29,15 +45,15 @@ void imprimirenpantalla() {
 }
 
 void loop() {
+
+  
 	delay(2000); //Es un sensor lento, por lo que hay que darle tiempo.
 	float h = dht.readHumidity();
 	float t = dht.readTemperature();
+  imprimirTyH(t, h); //imprimir en serial
 	if(isnan(h) || isnan(t)){ //Si no lee correctamente dar codigo error;
 		h = -1; //Codigo de error en la lectura humedad
-		t = -1; //Codigo de erro en la lectura temperatria
-		return;
-	}
-	//Si logra leer correctamente el codigo
-	
+		t = -1; //Codigo de erro en la lectura temperatria		
+	}		
 	
 }
